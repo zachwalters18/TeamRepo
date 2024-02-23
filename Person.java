6a7d99ca93d412b652eb6c42be793b3125064b69
zachwalters18 <zwalters@murraystate.edu>
2024-02-23 13:08:05 -0600

    no

---
+++ Person.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Person {
+    Scanner scn = new Scanner(System.in);
+
+    String name;
+    int age;
+
+    public void setName(String input_name){
+        System.out.println("Enter name: ");
+        
+
+    }
+}
